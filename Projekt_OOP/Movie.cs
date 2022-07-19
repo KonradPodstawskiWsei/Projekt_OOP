ba7643aca12ae442ea7512d3877074470179e9b0
Konrad Podstawski <Konrad.Podstawski@microsoft.wsei.edu.pl>
2022-07-19 20:13:05 +0200

    documantation for movie table

--- Projekt_OOP/Movie.cs
+++ Projekt_OOP/Movie.cs
@@ -2,27 +2,46 @@
 {
     public class Movie
     {
+        /// <summary>
+        /// reprezentacja w kodzie relacji do tabeli movieTypes
+        /// </summary>
         public MovieTypes movieTypes { get; set; }
+
+        /// <summary>
+        /// reprezentacja w kodzie relacji do tabeli movies
+        /// </summary>
         public Movies movies { get; set; }
 
+        /// <summary>
+        /// reprezentacja w kodzie tabeli ID
+        /// </summary>
         public int ID
         {
             get { return movies.FilmID; }
             set { movies.FilmID = value; }
         }
 
+        /// <summary>
+        /// reprezentacja w kodzie tabeli MovieType
+        /// </summary>
         public string MovieType
         {
             get { return movieTypes.TypeName; }
             set { movieTypes.TypeName = value; }
         }
 
+        /// <summary>
+        /// reprezentacja w kodzie tabeli MovieName
+        /// </summary>
         public string MovieName
         {
             get { return movies.MovieName; }
             set { movies.MovieName = value; }
         }
 
+        /// <summary>
+        /// reprezentacja w kodzie tabeli MovieLink
+        /// </summary>
         public string Movielink
         {
             get { return movies.MovieLink; }
