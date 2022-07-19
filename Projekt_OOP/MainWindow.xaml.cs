1f60174aeb2d818203e7d38579c405d541352521
Konrad Podstawski <Konrad.Podstawski@microsoft.wsei.edu.pl>
2022-07-19 19:46:46 +0200

    remove unuse libs

--- Projekt_OOP/MainWindow.xaml.cs
+++ Projekt_OOP/MainWindow.xaml.cs
@@ -4,14 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO;
 using System.Data.SqlClient;
 
@@ -79,11 +71,6 @@ namespace Projekt_OOP
             approve.ItemsSource = LikeMoviesName;
             Nonapprove.ItemsSource = nonLikeMoviesName;
             allList.ItemsSource = moviesTitle;
-        }
-
-        private void films_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
